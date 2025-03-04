950a4b7c43acde7b2b9db059e965b2b32a1bea39
AzizbekEngineer <azizbekengineer@gmail.com>
2025-03-04 20:12:09 +0500

    added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "./components/hero/Hero";
 import Concepts from "../../components/concepts/Concepts";
 import Contact from "../../components/contact/Contact";
+import Help from "../../components/help/Help";
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <Contact />
+      {/* <Contact /> */}
+      <Help />
     </div>
   );
 };
